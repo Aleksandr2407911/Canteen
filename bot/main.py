d6ea7f9736d3d4656d2feafee4e8fde2ee3bd02e
aleksandr2407911 <olexander.rabota@yandex.ru>
2024-04-11 15:51:51 +0300

    user's and admin's main menu

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,9 @@
-from aiogram.utils import executor
+import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from handlers.admin import main as admin_main
+from handlers.user import main as user_main
+from config import Config
+#функция конфигурирования и запуска бота
+async def main():
 
-from bot.filters import register_all_filters
-from bot.misc import TgKeys
-from bot.handlers import register_all_handlers
-from bot.database.models import register_models
-
-
-async def __on_start_up(dp: Dispatcher) -> None:
-    register_all_filters(dp)
-    register_all_handlers(dp)
-    register_models()
-
-
-def start_bot():
-    bot = Bot(token=TgKeys.TOKEN, parse_mode='HTML')
-    dp = Dispatcher(bot, storage=MemoryStorage())
-    executor.start_polling(dp, skip_updates=True, on_startup=__on_start_up)
+    #загружаем
